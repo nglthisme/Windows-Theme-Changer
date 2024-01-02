44cfa86e605b22c1f8269de14f4365a5013ea91e
Dodo <96394838+nglthisme@users.noreply.github.com>
2024-01-02 14:50:36 +0200

    Add files via upload
    The new Theme Selector can now run the commands at a certain time every day

--- Theme_Selector.py
+++ Theme_Selector.py
@@ -1,4 +1,18 @@
 ## theme selector
 import os
+from time import sleep
+from apscheduler.schedulers.background import BackgroundScheduler, BlockingScheduler
 
-os.popen(r"C:\Users\juanc\Themes\aero.theme")
+def joinlight():
+    os.popen(r"C:\Users\juanc\Themes\aero.theme")
+
+def embracedark():
+    os.popen(r"C:\Users\juanc\Themes\dark.theme")
+
+
+scheduler = BlockingScheduler()
+scheduler.add_job(embracedark, 'cron', hour = 19)
+scheduler.add_job(joinlight, 'cron', hour = 6)
+
+
+scheduler.start()
